04b2af189b71bf68ddfc810b1e85fe675b819009
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-06-27 17:14:59 +0200

    .

--- Website/src/components/dapi/Require.tsx
+++ Website/src/components/dapi/Require.tsx
@@ -1,17 +1,51 @@
 import { BuildConfig } from "@Native/BuildConfig";
 
 interface RequireProps extends React.PropsWithChildren {
-  version: string;
+  version?: string;
+  pattern?: string;
 }
 
-function Require(props: RequireProps) {
-  const { version } = props;
+const Require = (props: RequireProps) => {
+  const { version, pattern } = props;
 
-  if (BuildConfig.VERSION_CODE >= version) {
-    return <>{props.children}</>;
+  const compareVer = (ver1, middle, ver2) => {
+    const res = new Intl.Collator("en").compare(ver1, ver2);
+    let comp;
+
+    switch (middle) {
+      case "=":
+        comp = 0 === res;
+        break;
+
+      case ">":
+        comp = 1 === res;
+        break;
+
+      case ">=":
+        comp = 1 === res || 0 === res;
+        break;
+
+      case "<":
+        comp = -1 === res;
+        break;
+
+      case "<=":
+        comp = -1 === res || 0 === res;
+        break;
+    }
+
+    return comp;
+  };
+
+  if (!version) {
+    if (compareVer(BuildConfig.VERSION_NAME, pattern, version)) {
+      return <>{props.children}</>;
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
-}
+};
 
 export default Require;
